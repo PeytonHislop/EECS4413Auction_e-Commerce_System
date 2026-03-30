60dcce2439fa2c11a1201b85e5be5cd534715612
217761651 <158077739+217761651@users.noreply.github.com>
2026-03-30 11:47:47 -0400

    Create WebSocketConfig.java

---
+++ auction-service/src/main/java/com/code2cash/auction/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.code2cash.auction.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * WebSocket configuration for real-time auction updates
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // Enable a simple in-memory message broker
+        config.enableSimpleBroker("/topic");
+        // Set application destination prefix
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Register the WebSocket endpoint that clients will use to connect
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+}
